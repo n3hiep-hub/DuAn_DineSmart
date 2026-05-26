bfdcebda804d98cff03e70d49c7c366ee112424a
n3hiep-hub <n3hiep@gmail.com>
2026-05-26 11:35:36 +0700

    Fix served order status before payment

--- DuAn_DineSmart/BLL/TrangThaiDonHang.cs
+++ DuAn_DineSmart/BLL/TrangThaiDonHang.cs
@@ -5,6 +5,7 @@ namespace DuAn_DineSmart.BLL
         public const string ChoBep = "Chờ bếp";
         public const string DangLam = "Đang làm";
         public const string ChoPhucVu = "Chờ phục vụ";
+        public const string DaPhucVu = "Đã phục vụ";
         public const string HoanThanh = "Hoàn thành";
     }
 }
